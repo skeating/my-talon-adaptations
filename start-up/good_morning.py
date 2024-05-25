fdef51747348cd73886411539c9fac90ce5e9009
Sarah Keating <s.keating@ucl.ac.uk>
2024-05-25 16:47:23 +0100

    this runs but actually it's the point where it doesn't know what it's trying to open

---
+++ start-up/good_morning.py
@@ -0,0 +1,13 @@
+from talon import Module, actions
+
+
+mod = Module()
+
+
+@mod.action_class
+class Actions:
+    def good_morning():
+        """ starts all the applications I need for my day"""
+        actions.user.switcher_launch("slack")
+
+    
---
+++ start-up/good_morning.talon
@@ -0,0 +1,13 @@
+os: windows
+
+#app: chrome
+#app: microsoft_edge
+--
+
+good morning: 
+    user.good_morning()
+    # user.launch("gitkraken")
+    # sleep(200ms)
+    # user.good_morning("microsoft_edge")
+    # sleep(2ms)
+    
